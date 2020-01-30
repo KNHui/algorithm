0f711257adfcd5550cae448eb3523d4fc8161c24
KNHui <37471802+KNHui@users.noreply.github.com>
2020-01-30 16:36:47 +0900

    Add programmers 43162, 43165

---
+++ programmers/P43162.cpp
@@ -0,0 +1,50 @@
+/*
+    네트워크
+    https://programmers.co.kr/learn/courses/30/lessons/43162
+
+    BFS의 반환값을 answer에 더하는데,
+    BFS로 탐색하면서 방문하지 않았던 컴퓨터가 나타난다면 1을 리턴하고,
+    그렇지 않다면 새로운 네트워크가 아니므로 0을 리턴한다.
+*/
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int bfs(int index, vector<vector<int>> &computers, vector<vector<bool>> &visits)
+{
+    bool isNew;
+    queue<int> iq;
+
+    isNew = false;
+    iq.push(index);
+    while (!iq.empty())
+    {
+        int i = iq.front();
+        iq.pop();
+
+        for (size_t j = 0; j < computers[i].size(); ++j)
+        {
+            if (!visits[i][j] && computers[i][j])
+            {
+                isNew = true;
+                iq.push(j);
+                visits[i][j] = true;
+            }
+        }
+    }
+
+    return isNew ? 1 : 0;
+}
+int solution(int n, vector<vector<int>> computers)
+{
+    int answer = 0;
+    vector<bool> visit(n, false);
+    vector<vector<bool>> visits(n, visit);
+    for (int i = 0; i < n; ++i)
+    {
+        answer += bfs(i, computers, visits);
+    }
+    return answer;
+}
---
+++ programmers/P43165.cpp
@@ -0,0 +1,35 @@
+/*
+    타겟 넘버
+    https://programmers.co.kr/learn/courses/30/lessons/43165
+
+    DFS로 최종 인덱스까지 도달했을 때,
+    값이 타겟 넘버와 같다면 1을 리턴하고,
+    다르면 0을 리턴하면서 경우의 수를 쌓아나간다.
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int dfs(int total, size_t index, vector<int> &numbers, int target)
+{
+    int plusCase;
+    int minusCase;
+    if (index == numbers.size())
+    {
+        if (total == target)
+            return 1;
+        else
+            return 0;
+    }
+    plusCase = dfs(total + numbers[index], index + 1, numbers, target);
+    minusCase = dfs(total - numbers[index], index + 1, numbers, target);
+    return (plusCase + minusCase);
+}
+
+int solution(vector<int> numbers, int target)
+{
+    int answer = 0;
+    answer = dfs(0, 0, numbers, target);
+    return answer;
+}
