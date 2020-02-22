0ebe8062e3bb0615b40645f0af225a955116ee42
KNHui <37471802+KNHui@users.noreply.github.com>
2020-02-22 20:34:02 +0900

    Add Baekjoon 2357, 6549

--- Baekjoon/B2357.cpp
+++ Baekjoon/B2357.cpp
@@ -2,8 +2,14 @@
     최솟값과 최댓값
     https://www.acmicpc.net/problem/2357
 
-    최솟값과 최댓값을 둘 다 구해야 하기 때문에 구간 최소 트리를 사용하여 최솟값을 구하고
-    구간 최소 트리를 수정하여 구간 최대 트리를 만들어서 최댓값을 구하면 된다.
+    최솟값과 최댓값을 둘 다 구해야 하는 문제다.
+
+    구간 최소 트리를 수정하여 각 구간의 최솟값과 최댓값을 모두 가지도록 하였다.
+    그리고 최솟값과 최댓값을 위한 함수를 따로 만들어주었다.
+
+    입력되는 정수들은 1 이상 1,000,000,000 이하의 값을 가진다.
+    그래서 최댓값을 찾는 query_max 함수에서는 구간을 벗어나면 0을 리턴하고,
+    최솟값을 찾는 query_min 함수에서는 구간을 벗어나면 1,000,000,001을 리턴한다.
 */
 #include <vector>
 #include <iostream>
@@ -11,118 +17,107 @@
 
 using namespace std;
 
-struct SegmentTree_Min
+struct SegmentTree
 {
-    int n;
-    vector<int> range_min;
-    SegmentTree_Min(const vector<int> &arr)
-    {
-        n = arr.size();
-        range_min.resize(n * 4);
-        init_min(arr, 0, n - 1, 1);
-    }
-    
-    int init_min(const vector<int> &arr, int left, int right, int node)
-    {
-        if (left == right)
-            return range_min[node] = arr[left];
-        int mid = (left + right) / 2;
-        int left_min = init_min(arr, left, mid, node * 2);
-        int right_min = init_min(arr, mid + 1, right, node * 2 + 1);
-        
-        return range_min[node] = min(left_min, right_min);
-    }
-	
-    int query_min(int left, int right, int node, int node_left, int node_right)
-    {
-        if (right < node_left || node_right < left)
-            return 1000000001;
-        if (left <= node_left && node_right <= right)
-            return range_min[node];
-        int mid = (left + right) / 2;
-        int left_min = query_min(left, right, node * 2, node_left, mid);
-        int right_min = query_min(left, right, node * 2 + 1, mid + 1, node_right);
-        return min(left_min, right_min);
-    }
-    
-    int query_min(int left, int right)
+	int n;
+	vector<int> range_max;
+	vector<int> range_min;
+	SegmentTree(const vector<int> &arr)
+	{
+		n = arr.size();
+		range_max.resize(n * 4);
+		range_min.resize(n * 4);
+		init_max(arr, 0, n - 1, 1);
+		init_min(arr, 0, n - 1, 1);
+	}
+
+	int init_max(const vector<int> &arr, int left, int right, int node)
+	{
+		if (left == right)
+			return range_max[node] = arr[left];
+		int mid = (left + right) / 2;
+		int left_max = init_max(arr, left, mid, node * 2);
+		int right_max = init_max(arr, mid + 1, right, node * 2 + 1);
+		return range_max[node] = max(left_max, right_max);
+	}
+
+	int init_min(const vector<int> &arr, int left, int right, int node)
+	{
+		if (left == right)
+			return range_min[node] = arr[left];
+		int mid = (left + right) / 2;
+		int left_min = init_min(arr, left, mid, node * 2);
+		int right_min = init_min(arr, mid + 1, right, node * 2 + 1);
+
+		return range_min[node] = min(left_min, right_min);
+	}
+
+	int query_max(int left, int right, int node, int node_left, int node_right)
+	{
+		if (right < node_left || node_right < left)
+			return 0;
+		if (left <= node_left && node_right <= right)
+			return range_max[node];
+		int mid = (node_left + node_right) / 2;
+		int left_max = query_max(left, right, node * 2, node_left, mid);
+		int right_max = query_max(left, right, node * 2 + 1, mid + 1, node_right);
+		return max(left_max, right_max);
+	}
+
+	int query_max(int left, int right)
+	{
+		return query_max(left, right, 1, 0, n - 1);
+	}
+
+	int query_min(int left, int right, int node, int node_left, int node_right)
+	{
+		if (right < node_left || node_right < left)
+			return 1000000001;
+		if (left <= node_left && node_right <= right)
+			return range_min[node];
+		int mid = (node_left + node_right) / 2;
+		int left_min = query_min(left, right, node * 2, node_left, mid);
+		int right_min = query_min(left, right, node * 2 + 1, mid + 1, node_right);
+		return min(left_min, right_min);
+	}
+
+	int query_min(int left, int right)
 	{
 		return query_min(left, right, 1, 0, n - 1);
 	}
 };
 
-struct SegmentTree_Max
-{
-    int n;
-    vector<int> range_max;
-    SegmentTree_Max(const vector<int> &arr)
-    {
-        n = arr.size();
-        range_max.resize(n * 4);
-        init_max(arr, 0, n - 1, 1);
-    }
-    
-    int init_max(const vector<int> &arr, int left, int right, int node)
-    {
-        if (left == right)
-            return range_max[node] = arr[left];
-        int mid = (left + right) / 2;
-        int left_max = init_max(arr, left, mid, node * 2);
-        int right_max = init_max(arr, mid + 1, right, node * 2 + 1);
-        return range_max[node] = max(left_max, right_max);
-    }
-    
-    int query_max(int left, int right, int node, int node_left, int node_right)
-    {
-        if (right < node_left || node_right < left)
-            return -1;
-        if (left <= node_left && node_right <= right)
-            return range_max[node];
-        int mid = (left + right) / 2;
-        int left_max = query_max(left, right, node * 2, node_left, mid);
-        int right_max = query_max(left, right, node * 2 + 1, mid + 1, node_right);
-        return max(left_max, right_max);
-    }
-    
-    int query_max(int left, int right)
-	{
-		return query_max(left, right, 1, 0, n - 1);
-	}
-};
-
-
 int N, M;
-vector<int> nums;
 
-void init()
+void init(vector<int> &nums)
 {
-    cin >> N >> M;
-    for (int i = 0; i < N; ++i)
-    {
-        int num;
-        cin >> num;
-        nums.push_back(num);
-    }
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i)
+	{
+		int num;
+		cin >> num;
+		nums.push_back(num);
+	}
 }
 
-void solve()
+void solve(const vector<int> &nums)
 {
-    SegmentTree_Min st_min(nums);
-    SegmentTree_Max st_max(nums);
-    for (int i = 0; i < M; ++i)
-    {
-        int left, right;
-        cin >> left >> right;
-        cout << st_min.query_min(left - 1, right - 1) << ' ' << st_max.query_max(left - 1, right - 1) << '\n';
-    }
+	SegmentTree st(nums);
+	for (int i = 0; i < M; ++i)
+	{
+		int left, right;
+		cin >> left >> right;
+		cout << st.query_min(left - 1, right - 1) << ' ' << st.query_max(left - 1, right - 1) << '\n';
+	}
 }
 
 int main()
 {
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
-   
-   init();
-   solve();
-   return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	vector<int> nums;
+	init(nums);
+	solve(nums);
+	return 0;
 }
---
+++ Baekjoon/B6549.cpp
@@ -0,0 +1,118 @@
+/*
+	히스토그램에서 가장 큰 직사각형
+	https://www.acmicpc.net/problem/6549
+
+	분할 정복을 통하여 빠른 시간에 가장 큰 직사각형을 구하는 문제다.
+
+	세그먼트 트리를 사용하여 query_area 함수에서 구간 내 가장 큰 직사각형을 구한다.
+	이때, query_idx 함수를 사용하여 구간 내 가장 낮은 높이의 위치 idx를 구하고,
+	idx의 왼편, 현재 구간, idx의 오른편 중 가장 큰 직사각형의 넓이를 반환한다.
+	그 이유는 가장 낮은 높이가 있는 idx를 제외하고 idx의 왼편과 오른편을 확인하여,
+	더 높고 넓은 직사각형이 있는지 찾기 위해서다.
+
+	query_idx 함수에서는 구간을 벗어난 경우 MAX_IDX( = 100001)을 반환한다.
+	이 버려지는 값은 vector<int>& arr에 인덱스로 사용할 수 없으므로,
+	조건문을 사용하여 버린다.
+*/
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+constexpr auto MAX_IDX = 100001;
+
+struct SegmentTree
+{
+    int n;
+    vector<int> range_idx;
+    SegmentTree(const vector<int> &arr)
+    {
+        n = arr.size();
+        range_idx.resize(n * 4);
+        init(arr, 0, n - 1, 1);
+    }
+
+    int init(const vector<int> &arr, int left, int right, int node)
+    {
+        if (left == right)
+            return range_idx[node] = left;
+        int mid = (left + right) / 2;
+        int left_idx = init(arr, left, mid, node * 2);
+        int right_idx = init(arr, mid + 1, right, node * 2 + 1);
+        return range_idx[node] = arr[left_idx] < arr[right_idx] ? left_idx : right_idx;
+    }
+
+    int query_idx(const vector<int> &arr, int left, int right, int node, int node_left, int node_right)
+    {
+        if (right < node_left || node_right < left)
+            return MAX_IDX;
+        if (left <= node_left && node_right <= right)
+            return range_idx[node];
+        int mid = (node_left + node_right) / 2;
+        int left_idx = query_idx(arr, left, right, node * 2, node_left, mid);
+        int right_idx = query_idx(arr, left, right, node * 2 + 1, mid + 1, node_right);
+        if (left_idx == MAX_IDX)
+            return right_idx;
+        else if (right_idx == MAX_IDX)
+            return left_idx;
+        return arr[left_idx] < arr[right_idx] ? left_idx : right_idx;
+    }
+
+    int query_idx(const vector<int> &arr, int left, int right)
+    {
+        return query_idx(arr, left, right, 1, 0, n - 1);
+    }
+
+    ll query_area(const vector<int> &arr, ll left, ll right)
+    {
+        ll idx = (ll)query_idx(arr, (int)left, (int)right);
+        ll area = (ll)arr[(int)idx] * (right - left + 1);
+        if (left <= idx - 1)
+        {
+            ll left_area = query_area(arr, left, idx - 1);
+            area = max(area, left_area);
+        }
+        if (idx + 1 <= right)
+        {
+            ll right_area = query_area(arr, idx + 1, right);
+            area = max(area, right_area);
+        }
+        return area;
+    }
+};
+
+void input(vector<int> &rects, const int &n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        int rect;
+        cin >> rect;
+        rects.push_back(rect);
+    }
+}
+
+void solve()
+{
+    int n;
+
+    cin >> n;
+    while (n)
+    {
+        vector<int> rects;
+        input(rects, n);
+        SegmentTree st(rects);
+        cout << st.query_area(rects, 0, (ll)rects.size() - 1) << '\n';
+        cin >> n;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return 0;
+}
