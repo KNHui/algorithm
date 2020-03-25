a7bb0a48cfee5228c85e87c0424420371181e6bf
KNHui <southshinekim@gmail.com>
2020-03-25 19:43:48 +0900

    Add Baekjoon 1005

---
+++ Baekjoon/B1005.cpp
@@ -0,0 +1,87 @@
+/*
+	ACM Craft
+	https://www.acmicpc.net/problem/1005
+
+	"위상 정렬"을 사용하여, 목표 건물 'W'를 건설 완료 하는데 드는 최소 시간을 구하는 문제다.
+
+	필자는 "진입 차수 위상 정렬"을 사용했다.
+*/
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+constexpr int MAXN = 1000 + 1;
+
+int N, K;
+int W;
+int time[MAXN];
+int indegree[MAXN];
+vector<int> order[MAXN];
+
+void init()
+{
+	cin >> N >> K;
+	for (int i = 1; i <= N; ++i)
+		order[i].clear();
+	for (int i = 1; i <= N; ++i)
+		cin >> time[i];
+	memset(indegree, 0, sizeof(indegree));
+	for (int i = 1; i <= K; ++i)
+	{
+		int X, Y;
+
+		cin >> X >> Y;
+		order[X].push_back(Y);
+		++indegree[Y];
+	}
+	cin >> W;
+}
+
+int solve()
+{
+	vector<int> total_time(N + 1, 0);
+	queue<int> q;
+
+	for (int i = 1; i <= N; ++i)
+	{
+		if (indegree[i] == 0)
+		{
+			q.push(i);
+			total_time[i] = time[i];
+		}
+	}
+	while (!q.empty())
+	{
+		int building = q.front();
+
+		q.pop();
+		for (size_t i = 0; i < order[building].size(); ++i)
+		{
+			int next = order[building][i];
+
+			total_time[next] = max(total_time[next], total_time[building] + time[next]);
+			if (--indegree[next] == 0)
+				q.push(next);
+		}
+	}
+	return total_time[W];
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int tc;
+	cin >> tc;
+	for (int tcn = 1; tcn <= tc; ++tcn)
+	{
+		init();
+		cout << solve() << '\n';
+	}
+	return 0;
+}
