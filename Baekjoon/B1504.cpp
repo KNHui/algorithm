15e5d49e29522e8c900cb601cbc57b9b93e84a9b
KNHui <southshinekim@gmail.com>
2020-04-06 23:28:56 +0900

    Update Baekjoon 1504

--- Baekjoon/B1504.cpp
+++ Baekjoon/B1504.cpp
@@ -4,28 +4,29 @@
 
 	다익스트라 알고리즘을 사용하여 최단 경로를 찾는 문제다.
 
-	반드시 거쳐야 하는 정점이 두 개 주어지기 때문에, 다음과 같이 정점을 지나는 경우들을 처리해야 한다.
+	반드시 거쳐야 하는 정점이 두 개 주어지기 때문에, 다음과 같이 경우의 수를 처리했다.
 	1) 출발점 - 첫 번째 정점 - 두 번째 정점 - 도착점
 	2) 출발점 - 두 번째 정점 - 첫 번째 정점 - 도착점
-	위 같은 경우가 있는데 각 경로와 정답이 상수 MAXINT보다 크거나 음수인지 확인해서 유효한지 검사했다.
+	각 경로와 정답이 'INF(987654321)'보다 크거나 음수인지 확인해서 유효성을 검사했다.
 	만약 유효한 값들이라면 더 작은 값을 answer에 대입하고 마지막으로 answer가 유효한지 검사 후 출력한다.
 */
-#include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
-constexpr auto MAXINT = 987654321;
+constexpr auto INF = 987654321;
+constexpr auto MAXN = 800 + 1;
 
 int N, E;
-int map[801][801];
-int mid1, mid2;
-
 int answer;
-bool isValid(int val)
+int mid1, mid2;
+int map[MAXN][MAXN];
+
+bool is_valid(int val)
 {
-	if (0 <= val && val <= MAXINT)
+	if (val >= 0 && val <= INF)
 		return true;
 	return false;
 }
@@ -34,18 +35,15 @@ void init()
 {
 	for (int i = 0; i < N; ++i)
 	{
-		fill(map[i], map[i] + N, MAXINT);
+		fill(map[i], map[i] + N, INF);
 	}
-
 	answer = -1;
 }
 
 void input()
 {
 	cin >> N >> E;
-
 	init();
-
 	for (int i = 0; i < E; ++i)
 	{
 		int a, b, c;
@@ -61,7 +59,7 @@ void input()
 int dijkstra(int start, int goal)
 {
 	priority_queue<pair<int, int>, vector<pair<int, int>>> pq;
-	vector<int> min_dist(N, MAXINT);
+	vector<int> min_dist(N, INF);
 
 	pq.push({0, start});
 	min_dist[start] = 0;
@@ -98,14 +96,14 @@ void solve()
 	M1toE = dijkstra(mid1, N - 1);
 	M2toE = dijkstra(mid2, N - 1);
 
-	int candi1 = MAXINT + 1;
-	int candi2 = MAXINT + 1;
+	int candi1 = INF + 1;
+	int candi2 = INF + 1;
 
-	if (isValid(MtoM))
+	if (is_valid(MtoM))
 	{
-		if (isValid(StoM1) && isValid(M2toE))
+		if (is_valid(StoM1) && is_valid(M2toE))
 			candi1 = StoM1 + MtoM + M2toE;
-		if (isValid(StoM2) && isValid(M1toE))
+		if (is_valid(StoM2) && is_valid(M1toE))
 			candi2 = StoM2 + MtoM + M1toE;
 		answer = candi1 < candi2 ? candi1 : candi2;
 	}
@@ -118,6 +116,6 @@ int main()
 
 	input();
 	solve();
-	cout << (isValid(answer) ? answer : -1) << '\n';
+	cout << (is_valid(answer) ? answer : -1) << '\n';
 	return 0;
 }
