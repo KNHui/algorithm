31740815744d2341a9c30203cebbef2036d1e2a8
KNHui <southshinekim@gmail.com>
2020-06-10 19:42:04 +0900

    Solve Baekjoon 19237

--- solve/B19237.cpp
+++ solve/B19237.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 
 using namespace std;
@@ -10,17 +11,10 @@ constexpr int dx[4] = { 0, 0, -1, 1 };
 
 int				N, M, k;
 int				dead_cnt;
-int				shark_d[MAX_M];											// 현재 방향
+int				shark_d[MAX_M];
 int				shark_priority[MAX_M][4][4];
 pair<int, int>	shark_pos[MAX_M];
-pair<int, int>	smell[MAX_N][MAX_N];									// 상어 인덱스, 남은 냄새
-
-// 계속 같은 패턴 반복하는 경우 걸러줘야 하는데 아래와 같이 사용하면 제출할 때 메모리 초과
-bool			visit1[MAX_M][MAX_N][MAX_N][4][MAX_N][MAX_N][MAX_k];	// 약 10억 개 배열
-//					  상어idx	y	  x	    d  냄새y   냄새x 냄새
-
-bool			visit2[MAX_M][MAX_N][MAX_N][4][MAX_k];					// 약 2천500만 개 배열
-//						idx		y		x	 d	냄새
+pair<int, int>	smell[MAX_N][MAX_N];
 
 void	init(void)
 {
@@ -36,11 +30,17 @@ void	init(void)
 			}
 		}
 	for (int idx = 1; idx <= M; ++idx)
+	{
 		cin >> shark_d[idx];
+		--shark_d[idx];
+	}
 	for (int idx = 1; idx <= M; ++idx)
 		for (int row = 0; row < 4; ++row)
 			for (int col = 0; col < 4; ++col)
+			{
 				cin >> shark_priority[idx][row][col];
+				--shark_priority[idx][row][col];
+			}
 }
 
 void	decrease(void)
@@ -60,8 +60,6 @@ bool	check_range(int y, int x)
 	return y >= 0 && y < N && x >= 0 && x < N;
 }
 
-// 1, 2, 3, 4는 각각 위, 아래, 왼쪽, 오른쪽
-// 상어가 움직이지 못 하는 경우는 없음
 void	move(void)
 {
 	for (int idx = 1; idx <= M; ++idx)
@@ -109,6 +107,7 @@ void	dead(void)
 {
 	bool is_there[MAX_N][MAX_N];
 
+	memset(is_there, false, sizeof(is_there));
 	for (int idx = 1; idx <= M; ++idx)
 	{
 		int&	y = shark_pos[idx].first;
@@ -129,84 +128,17 @@ void	dead(void)
 	}
 }
 
-// 원래는 idx 구간 [1, M]을 순회했다.
-// 그리고 상어의 y, x, d에 따라 모든 맵의 냄새을 확인 했다. M * N * N
-bool	is_done1(void)
-{
-	bool ret = true;
-
-	for (int idx = 1; idx <= M; ++idx)
-	{
-		int		y = shark_pos[idx].first;
-		int		x = shark_pos[idx].second;
-		int		d = shark_d[idx];
-
-		if (y == -1)
-			continue;
-		for (int row = 0; row < N; ++row)
-			for (int col = 0; col < N; ++col)
-			{
-				int	remain = smell[row][col].second;
-
-				if (!visit1[idx][y][x][d][row][col][remain])
-				{
-					ret = false;
-					visit1[idx][row][col][d][remain] = true;
-				}
-			}
-	}
-	for (int row = 0; row < N; ++row)
-		for (int col = 0; col < N; ++col)
-		{
-			int idx = smell[row][col].first;
-			int	d = smell[row][col].second;
-			int	remain = smell[row][col].second;
-
-			if (row == -1)
-				continue;
-			if (!visit2[idx][row][col][d][remain])
-			{
-				ret = false;
-				visit2[idx][row][col][d][remain] = true;
-			}
-		}
-	return ret;
-}
-
-bool	is_done2(void)
-{
-	bool ret = true;
-
-	for (int row = 0; row < N; ++row)
-		for (int col = 0; col < N; ++col)
-		{
-			int idx = smell[row][col].first;
-			int	d = smell[row][col].second;
-			int	remain = smell[row][col].second;
-
-			if (!visit2[idx][row][col][d][remain])
-			{
-				ret = false;
-				visit2[idx][row][col][d][remain] = true;
-			}
-		}
-	return ret;
-}
-
 int		solve(void)
 {
 	int time = 1;
 
 	for (; time <= 1000 && dead_cnt < M - 1; ++time)
 	{
-		decrease();
 		move();
+		decrease();
 		dead();
-		// 어떻게 중복처리를 해야할까?
-		//if (is_done())
-		//	return -1;
 	}
-	return dead_cnt == M - 1 ? time : -1;
+	return dead_cnt == M - 1 ? time - 1 : -1;
 }
 
 int		main(void)
